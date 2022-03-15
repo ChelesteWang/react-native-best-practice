95a9d301bc0078c5d83e2609a25a580acc87d4e7
ChelesteWang <1581446178@qq.com>
2022-03-15 08:46:41 +0800

    feat: 优化路由拆分组件

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,23 +1,8 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from './pages/Home';
+import SettingsScreen from './pages/Settings';
 
 const Tab = createBottomTabNavigator();
 
